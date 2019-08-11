b9ad48be359ebba257254db165c0f6a69ee8ab46
Jim Rice <hitjim@gmail.com>
2019-08-10 20:53:19 -0500

    first baby step to creating pdfs (#20)
    * got pdf function stubbed out, generating pdfs
    
    * handle bill description and print to PDF
    
    * get started with pdf generation, prep types for calculating and reporting individual usage on pdf
    
    * stop manually building filepaths like a wild animal
    
    * handle all the Extra types, calculate the right way, fix tests
    
    * switch pdf library to gofpdf
    
    * switched to better pdf library and sketched table-to-pdf function
    
    * comments for required tables

--- ting-bill-split.go
+++ ting-bill-split.go
@@ -9,51 +9,62 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 
 	"github.com/BurntSushi/toml"
+	"github.com/jung-kurt/gofpdf"
 	"github.com/shopspring/decimal"
 )
 
 type bill struct {
-	Minutes      float64  `toml:"minutes"`
-	Messages     float64  `toml:"messages"`
-	Megabytes    float64  `toml:"megabytes"`
-	Devices      float64  `toml:"devices"`
-	Extras       float64  `toml:"extras"`
-	Fees         float64  `toml:"fees"`
-	DeviceIds    []string `toml:"deviceIds"`
-	ShortStrawID string   `toml:"shortStrawId"`
-	Total        float64  `toml:"total"`
+	Minutes        float64  `toml:"minutes"`
+	Messages       float64  `toml:"messages"`
+	Megabytes      float64  `toml:"megabytes"`
+	Devices        float64  `toml:"devices"`
+	ExtraMinutes   float64  `toml:"extraMinutes"`
+	ExtraMessages  float64  `toml:"extraMessages"`
+	ExtraMegabytes float64  `toml:"extraMegabytes"`
+	Fees           float64  `toml:"fees"`
+	DeviceIds      []string `toml:"deviceIds"`
+	ShortStrawID   string   `toml:"shortStrawId"`
+	Total          float64  `toml:"total"`
+	Description    string   `toml:"description"`
 }
 
 // Used to contain all subtotals for a monthly bill.
-// MinSubs, MsgSubs, MegSubs are maps of decimal.Decimal totals.
+// MinuteCosts, MessageCosts, MegabyteCosts are maps of decimal.Decimal totals.
 // They are split by bill.DeviceIds and calculated by usage in parseMaps.
-// DeltaSubs reflect the rest of the items not based on usage, which get split evenly across all deviceIds
+// SharedCosts reflect the rest of the items not based on usage, which get split evenly across all deviceIds
 type billSplit struct {
-	MinSubs   map[string]decimal.Decimal
-	MsgSubs   map[string]decimal.Decimal
-	MegSubs   map[string]decimal.Decimal
-	DeltaSubs map[string]decimal.Decimal
+	MinuteCosts   map[string]decimal.Decimal
+	MinuteQty     map[string]int
+	MessageCosts  map[string]decimal.Decimal
+	MessageQty    map[string]int
+	MegabyteCosts map[string]decimal.Decimal
+	MegabyteQty   map[string]int
+	SharedCosts   map[string]decimal.Decimal
 }
 
 func parseMaps(min map[string]int, msg map[string]int, meg map[string]int, bil bill) (billSplit, error) {
 	bs := billSplit{
 		make(map[string]decimal.Decimal),
+		make(map[string]int),
 		make(map[string]decimal.Decimal),
+		make(map[string]int),
 		make(map[string]decimal.Decimal),
+		make(map[string]int),
 		make(map[string]decimal.Decimal),
 	}
 	var usedMin, usedMsg, usedMeg int
 	DecimalPrecision := int32(6)
 	RoundPrecision := int32(2)
 
-	bilMinutes := decimal.NewFromFloat(bil.Minutes)
-	bilMessages := decimal.NewFromFloat(bil.Messages)
-	bilMegabytes := decimal.NewFromFloat(bil.Megabytes)
-	delta := decimal.NewFromFloat(bil.Devices + bil.Extras + bil.Fees).Round(DecimalPrecision)
+	bilMinutes := decimal.NewFromFloat(bil.Minutes + bil.ExtraMinutes)
+	bilMessages := decimal.NewFromFloat(bil.Messages + bil.ExtraMessages)
+	bilMegabytes := decimal.NewFromFloat(bil.Megabytes + bil.ExtraMegabytes)
+	delta := decimal.NewFromFloat(bil.Devices + bil.Fees).Round(DecimalPrecision)
 	deviceQty := decimal.New(int64(len(bil.DeviceIds)), 0)
 
 	// Calculate usage totals
@@ -73,71 +84,89 @@ func parseMaps(min map[string]int, msg map[string]int, meg map[string]int, bil b
 		subMin := decimal.New(int64(min[id]), DecimalPrecision)
 		totalMin := decimal.New(int64(usedMin), DecimalPrecision)
 		percentMin := subMin.DivRound(totalMin, DecimalPrecision)
-		bs.MinSubs[id] = percentMin.Mul(bilMinutes).Round(RoundPrecision)
+		bs.MinuteCosts[id] = percentMin.Mul(bilMinutes).Round(RoundPrecision)
+		// It's possible for a device to still be on the bill, but not show any usage data
+		if value, exists := min[id]; exists {
+			bs.MinuteQty[id] = value
+		} else {
+			bs.MinuteQty[id] = 0
+		}
 
 		subMsg := decimal.New(int64(msg[id]), DecimalPrecision)
 		totalMsg := decimal.New(int64(usedMsg), DecimalPrecision)
 		percentMsg := subMsg.DivRound(totalMsg, DecimalPrecision)
-		bs.MsgSubs[id] = percentMsg.Mul(bilMessages).Round(RoundPrecision)
+		bs.MessageCosts[id] = percentMsg.Mul(bilMessages).Round(RoundPrecision)
+		// It's possible for a device to still be on the bill, but not show any usage data
+		if value, exists := msg[id]; exists {
+			bs.MessageQty[id] = value
+		} else {
+			bs.MessageQty[id] = 0
+		}
 
 		subMeg := decimal.New(int64(meg[id]), DecimalPrecision)
 		totalMeg := decimal.New(int64(usedMeg), DecimalPrecision)
 		percentMeg := subMeg.DivRound(totalMeg, DecimalPrecision)
-		bs.MegSubs[id] = percentMeg.Mul(bilMegabytes).Round(RoundPrecision)
+		bs.MegabyteCosts[id] = percentMeg.Mul(bilMegabytes).Round(RoundPrecision)
+		// It's possible for a device to still be on the bill, but not show any usage data
+		if value, exists := meg[id]; exists {
+			bs.MegabyteQty[id] = value
+		} else {
+			bs.MegabyteQty[id] = 0
+		}
 
-		bs.DeltaSubs[id] = delta.DivRound(deviceQty, RoundPrecision)
+		bs.SharedCosts[id] = delta.DivRound(deviceQty, RoundPrecision)
 	}
 
 	minSubSum := decimal.New(0, RoundPrecision)
-	for _, sub := range bs.MinSubs {
+	for _, sub := range bs.MinuteCosts {
 		minSubSum = minSubSum.Add(sub)
 	}
 
 	minSubExtra := bilMinutes.Sub(minSubSum)
 	if minSubExtra.GreaterThan(decimal.New(0, RoundPrecision)) {
 		fmt.Printf("Remainder minutes cost of $%s to deviceId %s\n", minSubExtra.String(), bil.ShortStrawID)
-		bs.MinSubs[bil.ShortStrawID].Add(minSubExtra)
+		bs.MinuteCosts[bil.ShortStrawID].Add(minSubExtra)
 	} else {
 		fmt.Println("There was no remainder cost when splitting minutes.")
 	}
 
 	msgSubSum := decimal.New(0, RoundPrecision)
-	for _, sub := range bs.MsgSubs {
+	for _, sub := range bs.MessageCosts {
 		msgSubSum = msgSubSum.Add(sub)
 	}
 
 	msgSubExtra := bilMessages.Sub(msgSubSum)
 	if msgSubExtra.GreaterThan(decimal.New(0, RoundPrecision)) {
 		fmt.Printf("Remainder messages cost of $%s to deviceId %s\n", msgSubExtra.String(), bil.ShortStrawID)
-		bs.MsgSubs[bil.ShortStrawID].Add(msgSubExtra)
+		bs.MessageCosts[bil.ShortStrawID].Add(msgSubExtra)
 	} else {
 		fmt.Println("There was no remainder cost when splitting messages.")
 	}
 
 	megSubSum := decimal.New(0, RoundPrecision)
-	for _, sub := range bs.MegSubs {
+	for _, sub := range bs.MegabyteCosts {
 		megSubSum = megSubSum.Add(sub)
 	}
 
 	megSubExtra := bilMessages.Sub(megSubSum)
 	if megSubExtra.GreaterThan(decimal.New(0, RoundPrecision)) {
 		fmt.Printf("Remainder megabytes cost of $%s to deviceId %s\n", megSubExtra.String(), bil.ShortStrawID)
-		bs.MegSubs[bil.ShortStrawID].Add(megSubExtra)
+		bs.MegabyteCosts[bil.ShortStrawID].Add(megSubExtra)
 	} else {
 		fmt.Println("There was no remainder cost when splitting megabytes.")
 	}
 
 	deltaSubSum := decimal.New(0, RoundPrecision)
-	fmt.Println("bs.DeltaSubs is")
-	fmt.Println(bs.DeltaSubs)
-	for _, sub := range bs.DeltaSubs {
+	fmt.Println("bs.SharedCosts is")
+	fmt.Println(bs.SharedCosts)
+	for _, sub := range bs.SharedCosts {
 		deltaSubSum = deltaSubSum.Add(sub)
 	}
 
 	deltaSubExtra := delta.Sub(deltaSubSum)
 	if deltaSubExtra.GreaterThan(decimal.New(0, RoundPrecision)) {
 		fmt.Printf("Remainder delta cost of $%s added to deviceId %s\n", deltaSubExtra.String(), bil.ShortStrawID)
-		bs.DeltaSubs[bil.ShortStrawID] = bs.DeltaSubs[bil.ShortStrawID].Add(deltaSubExtra)
+		bs.SharedCosts[bil.ShortStrawID] = bs.SharedCosts[bil.ShortStrawID].Add(deltaSubExtra)
 	} else {
 		fmt.Println("There was no remainder cost when splitting delta.")
 	}
@@ -336,13 +365,12 @@ func createNewBillingDir(args []string) {
 			fmt.Printf("\n1. Enter values for the bills.toml file in new directory `%s`\n", newDirName)
 			fmt.Println("2. Add csv files for minutes, message, megabytes in the new directory")
 			fmt.Printf("3. run `ting-bill-split %s`\n", newDirName)
-
 		}
 	}
 }
 
 func createBillsFile(path string) {
-	path += "/bills.toml"
+	path += "/bill.toml"
 	f, err := os.Create(path)
 
 	if err != nil {
@@ -350,15 +378,18 @@ func createBillsFile(path string) {
 	}
 
 	newBills := bill{
-		Minutes:      0.00,
-		Messages:     0.00,
-		Megabytes:    0.00,
-		Devices:      0.00,
-		Extras:       0.00,
-		Fees:         0.00,
-		DeviceIds:    []string{},
-		ShortStrawID: "",
-		Total:        0.00,
+		Minutes:        0.00,
+		Messages:       0.00,
+		Megabytes:      0.00,
+		Devices:        0.00,
+		ExtraMinutes:   0.00,
+		ExtraMessages:  0.00,
+		ExtraMegabytes: 0.00,
+		Fees:           0.00,
+		DeviceIds:      []string{"1112223333", "2229998888", "etc"},
+		ShortStrawID:   "1112223333",
+		Total:          0.00,
+		Description:    "Ting Bill YYYY-MM-DD",
 	}
 
 	if err := toml.NewEncoder(f).Encode(newBills); err != nil {
@@ -369,6 +400,7 @@ func createBillsFile(path string) {
 // For a fileName string, return true if it contains the nameTerm anywhere.
 // If an empty string is provided for `ext`, no extension matching is performed.
 // Otherwise additional file extension matching is performed.
+// TODO LATER - maybe use path/filepath.Match
 func isFileMatch(fileName string, nameTerm string, ext string) bool {
 	r := regexp.MustCompile(`(?i)^[\w-]*` + nameTerm + `[\w-]*$`)
 
@@ -396,7 +428,7 @@ func parseDir(path string) {
 			continue
 		}
 
-		if billFile == nil && isFileMatch(file.Name(), "bills", "toml") {
+		if billFile == nil && isFileMatch(file.Name(), "bill", "toml") {
 			billFile, err = os.Open(file.Name())
 			if err != nil {
 				log.Fatal(err)
@@ -455,10 +487,6 @@ func parseDir(path string) {
 			log.Fatal(err)
 		}
 
-		// TODO remove this once we do something useful with it.
-		fmt.Println("billData ... something something ... *wanders off*")
-		fmt.Println(billData)
-
 		minMap, err := parseMinutes(minFile)
 		if err != nil {
 			log.Fatal(err)
@@ -483,19 +511,108 @@ func parseDir(path string) {
 			log.Fatal(err)
 		}
 		fmt.Println(split)
+
+		pdfFilePath := filepath.Join(path, billData.Description+".pdf")
+
+		invoiceNames, err := generatePDF(split, billData, pdfFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Invoice generation complete: %s", invoiceNames)
 	}
+}
+
+func generatePDF(bs billSplit, b bill, filePath string) (string, error) {
+	fmt.Printf("Generating invoice %s\n\n", filePath)
+	header := []string{"header1", "header2", "header3", "header4"}
+	type country struct {
+		nameStr, capitalStr, smellStr, birdStr string
+	}
+
+	countryList := []country{
+		{"country1", "capital1", "smell1", "bird1"},
+		{"country2", "capital2", "smell2", "bird2"},
+		{"c3", "cap2", "sm2", "brd2"},
+		{"Jupiter", "Mars", "farts", "toots"},
+	}
+
+	// Top Heading: bill title with invoice date; Device qty; Bill total; split total, Usage subtotal $, Devices subtotal $, Tax+reg subtotal
+
+	// Table 1: Usage
+	// heading: number, nickname?, min, msg, data (KB), min%, msg%, data%
+	// Then entries for each number
+	// then entry for "Total" under nickname, and rest of sums
+
+	// Table 2: Weighted costs
+	// heading: Weighted Costs: Minutes, Messages, Data
+	// Base: $x, $y, $z
+	// Extra: etc
+	// Total: etc
+
+	// Table 3: Shared costs
+	// heading: Type, Amount
+
+	// Table 4: Costs split
+	// heading: number, Nickname, Min, Msg, Data, Shared, Total
+	// entry for each number
+
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, b.Description)
+
+	improvedTable := func() {
+		// Column widths
+		w := []float64{40.0, 35.0, 40.0, 45.0}
+		wSum := 0.0
+		for _, v := range w {
+			wSum += v
+		}
+		left := (210 - wSum) / 2
+		// 	Header
+		pdf.SetY(20)
+		pdf.SetX(left)
+		for j, str := range header {
+			pdf.CellFormat(w[j], 7, str, "1", 0, "C", false, 0, "")
+		}
+		pdf.Ln(-1)
+		// Data
+		for _, c := range countryList {
+			pdf.SetX(left)
+			pdf.CellFormat(w[0], 6, c.nameStr, "LR", 0, "", false, 0, "")
+			pdf.CellFormat(w[1], 6, c.capitalStr, "LR", 0, "", false, 0, "")
+			pdf.CellFormat(w[2], 6, c.smellStr, "LR", 0, "R", false, 0, "")
+			pdf.CellFormat(w[3], 6, c.birdStr, "LR", 0, "R", false, 0, "")
+			pdf.Ln(-1)
+		}
+		pdf.SetX(left)
+		pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
+	}
+
+	improvedTable()
+
+	err := pdf.OutputFileAndClose(filePath)
+
+	// TODO LATER - add dates to bill. For now, entering manually in the "description" field in bill.toml
+	// Future: generate a range off min/max dates in usage files?
+	// Or maybe just have a new field in toml?
+
+	// TODO make this take a path in, for dir-mode splitting
+
+	return filePath, err
 }
 
 func main() {
 	fmt.Printf("Ting Bill Splitter\n\n")
 
-	billPtr := flag.String("bills", "", "filename for bills toml - ex: -bills=\"bills.toml\"")
+	billPtr := flag.String("bill", "", "filename for bill toml - ex: -bill=\"bill.toml\"")
 	minPtr := flag.String("minutes", "", "filename for minutes csv - ex: -minutes=\"minutes.csv\"")
 	msgPtr := flag.String("messages", "", "filename for messages csv - ex: -messages=\"messages.csv\"")
 	megPtr := flag.String("megabytes", "", "filename for megabytes csv - ex: -megabytes=\"megabytes.csv\"")
 
 	flag.Parse()
 	args := flag.Args()
+	targetDir := "."
 
 	if len(args) > 0 {
 		fmt.Println("Running in batch mode")
@@ -506,15 +623,16 @@ func main() {
 		case "new":
 			createNewBillingDir(args)
 		case "dir":
-			targetDir := "."
 			if len(args) > 1 {
 				targetDir = args[1]
 			}
 			fmt.Printf("\n targetDir is %s\n", targetDir)
 			parseDir(targetDir)
+			// TODO - make parseDir return a split, since non-dir parsing uses a split
+			//   then handle all actions after the if/else
 		default:
 			fmt.Println("Use `ting-bill-split new` or `new <billing-directory>` to create a new billing directory")
-			fmt.Println("Use `ting-bill-split dir <billing-directory>` to run on a directory containing a `bills.toml`, and CSV files for minutes, messages, and megabytes usage.")
+			fmt.Println("Use `ting-bill-split dir <billing-directory>` to run on a directory containing a `bill.toml`, and CSV files for minutes, messages, and megabytes usage.")
 			fmt.Println("  Each of these files must contain their type somewhere in the filename - i.e. `YYYYMMDD-messages.csv` or `messages-potatosalad.csv` or whatever.")
 			fmt.Printf("\n... or `-h` for flag options")
 		}
@@ -523,7 +641,7 @@ func main() {
 
 		badParam := false
 		paramMap := map[string]*string{
-			"bills":     billPtr,
+			"bill":      billPtr,
 			"minutes":   minPtr,
 			"messages":  msgPtr,
 			"megabytes": megPtr,
